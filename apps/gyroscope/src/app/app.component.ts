7561a12ed8666362c06ea27e09493f3d4621e50e
matthiasgekiere <matthias.gekiere@rydoo.com>
2020-02-12 16:01:16 +0100

    initial commit

---
+++ apps/gyroscope/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rydoo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gyroscope';
+}
