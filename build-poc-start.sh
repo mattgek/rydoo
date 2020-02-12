1113782496779958afe330431e5af20dc3d21aec
matthiasgekiere <matthias.gekiere@rydoo.com>
2020-02-12 17:23:35 +0100

    setup again

--- build-poc-start.sh
+++ build-poc-start.sh
@@ -7,7 +7,7 @@ ng g @nrwl/angular:application hotel --style=scss --routing
 
 # CREATING UI LIBRARY
 ng g @nrwl/angular:lib --directory=libs ui-compass
-ng g @nrwl/angular:lib --project=ui-compass --export
+ng g @nrwl/angular:component --project=ui-compass --export
 
 # CREATING CORE LIBRARY
 mkdir libs/core
@@ -15,9 +15,6 @@ ng g @nrwl/angular:lib --directory=core ui-core
 ng g @nrwl/angular:lib --directory=core app-core
 ng g @nrwl/angular:lib --project=app-core translations
 ng g @nrwl/angular:lib --project=app-core query-params
-
-# CREATING API LIBRARY
-ng g @nrwl/angular:lib --directory=api data-api
 
 # CREATING API LIBRARY
 ng g @nrwl/angular:lib --directory=api data-api
