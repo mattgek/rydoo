236e840347f8d509f3af0925fd54a87839fcca99
matthiasgekiere <matthias.gekiere@rydoo.com>
2020-02-13 10:40:39 +0100

    multi project

--- apps/flight/src/app/flight-app.module.ts
+++ apps/flight/src/app/flight-app.module.ts
@@ -2,11 +2,14 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    environment.production || !environment.buildIn ? BrowserModule : [],
     RouterModule.forChild([
       {
         path: 'flights',
--- apps/payment-methods/src/app/payment-method-app.module.ts
+++ apps/payment-methods/src/app/payment-method-app.module.ts
@@ -2,10 +2,13 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { environment } from '../../../flight/src/environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    environment.production || !environment.buildIn ? BrowserModule : [],
     RouterModule.forChild([
       {
         path: 'payment-methods',
